b7985c265ecef852c45ed73a5be01b25cbef3160
mojtaba <mojtaba75rs@gmail.com>
2023-03-29 01:21:52 +0430

    Refactoring the index component related to the Todos file

---
+++ src/components/Todos/index.jsx
@@ -0,0 +1,8 @@
+export { default as Todo } from "./Todo";
+export { default as Todos } from "./Todos";
+export { default as AddTodo } from "./AddTodo";
+export { default as SearchTodos } from "./SearchTodos";
+export { default as CategoryTodos } from "./CategoryTodos";
+export { default as TodosContainer } from "./TodosContainer";
+export { default as ClearListButton } from "./ClearListButton";
+export { default as SearchAndCategoryContainer } from "./SearchAndCategoryContainer";
--- src/components/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as AddTodo } from "./Todo/AddTodo";
-export { default as Todos } from "./Todo/Todos";
-export { default as Todo } from "./Todo/Todo";
