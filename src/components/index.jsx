463dd3901e609a41d864ae50dc1dd01688bd80e4
mojtaba <mojtaba75rs@gmail.com>
2023-03-28 23:16:33 +0430

    Remove the Alert component and use react-toastify
    Signed-off-by: mojtaba <mojtaba75rs@gmail.com>

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,3 @@
 export { default as AddTodo } from "./Todo/AddTodo";
 export { default as Todos } from "./Todo/Todos";
 export { default as Todo } from "./Todo/Todo";
-export { default as Alert } from "./Alert";
