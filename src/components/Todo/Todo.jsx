08fc89759cca4c9f94d56997032892a7f367adea
mojtaba <mojtaba75rs@gmail.com>
2023-03-28 23:20:20 +0430

    Write the code to complete the task

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,8 +1,18 @@
-const Todo = ({ text, updated, deleted }) => {
+const Todo = ({ text, completed, updated, deleted, onCompleted }) => {
   return (
     <>
-      <li className="list-item">
-        <span className="text">{text}</span>
+      <li className={`list-item ${completed ? "completed" : null}`}>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <input
+            type="checkbox"
+            className="checkbox-btn"
+            onClick={onCompleted}
+            defaultChecked={completed}
+          />
+          <span className="text">
+            {completed ? <del className="text">{text}</del> : text}
+          </span>
+        </div>
         <span className="btn-cotainer">
           <button className="edit-btn" onClick={updated}>
             <i className="fas fa-edit" />
